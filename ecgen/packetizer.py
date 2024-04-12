673f486ada7c1b43676ad79a210e8932b2dd3756
Chan Ming Han <chanminghan00@gmail.com>
2024-04-12 14:59:48 -0700

    feat: ecc wip, sender, recv, router classes

---
+++ ecgen/packetizer.py
@@ -0,0 +1,27 @@
+class Packetizer:
+    """
+    The Packetizer class is reponsible for breaking down an input stream into chunks, and padding them to maxBlockSize.
+    It is also responsible for converting each of these chunks into a bytestring for processing downstream.
+    utf-8 encoding is used for str->byte conversion.
+    """
+
+    def __init__(self, maxBlockSize=64):
+        self.maxBlockSize = maxBlockSize
+
+    def addHeader(self):
+        pass
+
+    def parse(self, input_stream):
+        packets = []
+        for i in range(len(input_stream) // self.maxBlockSize + 1):
+            packet = self.pad(input_stream[i*self.maxBlockSize: (i+1)*self.maxBlockSize])
+            packets.append(bytes(packet, "utf-8"))
+
+        return packets
+    
+    def pad(self, packet):
+        assert len(packet) <= self.maxBlockSize
+        return packet + '0' * (self.maxBlockSize - len(packet))
+
+    
+
---
+++ ecgen/utils.py
@@ -0,0 +1,38 @@
+import zlib
+class udphdr:
+    def __init__(self, sourcePort, destinationPort, udpLength, checkSum):
+        self.sourcePort = sourcePort
+        self.destinationPort = destinationPort
+        self.udpLength = udpLength
+        self.checkSum = checkSum
+
+class iphdr:
+    def __init__(self, sourceIP, destIP):
+        self.sourceIP = sourceIP
+        self.destIP = destIP
+
+
+class Packet:
+    """
+    This is more of a "struct" class created to abstract away packet logic.
+    Each packet is assigned a packetNumber, which is required by the zfec decoder.
+    """
+    def __init__(self, packetData: bytes, packetNumber: int, sourcePort: int, destinationPort: int, udpLength: int, sourceIP:str, destIP:str):
+        """
+        packetData: data of the packet in bytes
+        packetNumber: number of the packet
+        sourcePort: sending port
+        destinationPort: final destinationPort
+        udpLength: length of the packetData
+        """
+        self.packetData = packetData
+        self.packetNumber = packetNumber
+        checkSum = self.get_checksum(self.packetData + bytes(packetNumber) + bytes(sourcePort) + bytes(destinationPort) + bytes(udpLength)) # Generate a checksum across the packetData, packetNumber, source/destination ports, and udplength
+        self.udpheader = udphdr(sourcePort, destinationPort, udpLength, checkSum)
+        self.iphdr = iphdr(sourceIP, destIP)
+
+    def get_checksum(self, data):
+        return zlib.crc32(data)
+
+    def printPacket(self):
+        print(f"===== PACKET {self.packetNumber} ===== \n IPHEADER(srcIP: {self.iphdr.sourceIP} \t destIP: {self.iphdr.destIP}) \n UDPHEADER (srcPort: {self.udpheader.sourcePort} \t destPort: {self.udpheader.destinationPort} \t udpLen: {self.udpheader.udpLength} \t checkSum: {self.udpheader.checkSum}) \n BODY: {self.packetData}\n")
